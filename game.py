efe5b109ed2b90b1f455bba18cf88a0e55f94b36
roberta <roberta.mangiapane@gmail.com>
2020-01-14 11:01:56 +0000

    add switch_turn and declare_winner tests

--- game.py
+++ game.py
@@ -5,24 +5,47 @@ class Game:
 
     def __init__(self):
         self.player_name = ""
+        self.turn = "%s's turn" % self.player_name
         self.players = ["Player", "Computer"]
-        self.turn = ""
         self.options = ["Rock", "Paper", "Scissors"]
         self.player_choice = ""
         self.computer_choice = ""
+        self.winner = ""
 
     def insert_player_game(self, name):
         self.player_name = name
+
         return self.player_name
 
-    def start_game(self):
-        self.turn = random.choice(self.players)
+    def switch_turn(self):
+        if self.turn != "Computer's turn":
+            self.turn = "Computer's turn"
+        else:
+            self.turn = "%s's turn" % self.player_name
+
         return self.turn
 
     def player_turn_option(self, choice):
         self.player_choice = choice
+        self.switch_turn()
+
         return self.player_choice
 
     def computer_turn_option(self):
         self.computer_choice = random.choice(self.options)
+        self.switch_turn()
+
         return self.computer_choice
+
+    def declare_winner(self):
+
+        if self.player_choice == self.computer_choice:
+            self.winner = "It's a tie!"
+        elif self.player_choice == "Paper" and self.computer_choice == "Rock" or\
+                self.player_choice == "Rock" and self.computer_choice == "Scissors" or\
+                self.player_choice == "Scissors" and self.computer_choice == "Paper":
+            self.winner = "%s win!" % self.player_name
+        else:
+            self.winner = "Computer win!"
+
+        return self.winner
--- tests/game_test.py
+++ tests/game_test.py
@@ -11,9 +11,15 @@ class TestGame(unittest.TestCase):
     def test_insert_player_name(self):
         self.assertEqual(self.game.insert_player_game("Name"), "Name")
 
-    def test_start_game_random_player(self):
-        self.game.start_game = MagicMock(return_value="Player")
-        self.assertEqual(self.game.start_game(), "Player")
+    def test_switch_turn_computer(self):
+        self.game.player_name = "Player"
+        self.game.turn = "Player's turn"
+        self.assertEqual(self.game.switch_turn(), "Computer's turn")
+
+    def test_switch_turn_player(self):
+        self.game.player_name = "Player"
+        self.game.turn = "Computer's turn"
+        self.assertEqual(self.game.switch_turn(), "Player's turn")
 
     def test_start_game_random_computer(self):
         self.game.start_game = MagicMock(return_value="Computer")
@@ -22,9 +28,38 @@ class TestGame(unittest.TestCase):
     def test_player_turn_option_paper(self):
         self.assertEqual(self.game.player_turn_option("Paper"), "Paper")
 
-    def test_computer_turn_option_paper(self):
-        self.game.computer_turn_choice = MagicMock(return_value="Rock")
+    def test_computer_turn_random_option(self):
+        self.game.computer_turn_option = MagicMock(return_value="Rock")
         self.assertEqual(self.game.computer_turn_option(), "Rock")
+
+    def test_declare_winner_tie(self):
+        self.game.player_choice = "Rock"
+        self.game.computer_choice = "Rock"
+        self.assertEqual(self.game.declare_winner(), "It's a tie!")
+
+    def test_declare_winner_player(self):
+        self.game.player_name = "Player"
+        self.game.player_choice = "Paper"
+        self.game.computer_choice = "Rock"
+        self.assertEqual(self.game.declare_winner(), "Player win!")
+
+    def test_declare_winner_player2(self):
+        self.game.player_name = "Player"
+        self.game.player_choice = "Rock"
+        self.game.computer_choice = "Scissors"
+        self.assertEqual(self.game.declare_winner(), "Player win!")
+
+    def test_declare_winner_player3(self):
+        self.game.player_name = "Player"
+        self.game.player_choice = "Scissors"
+        self.game.computer_choice = "Paper"
+        self.assertEqual(self.game.declare_winner(), "Player win!")
+
+    def test_declare_winner_computer(self):
+        self.game.player_name = "Player"
+        self.game.player_choice = "Rock"
+        self.game.computer_choice = "Paper"
+        self.assertEqual(self.game.declare_winner(), "Computer win!")
 
 
 if __name__ == '__main__':
