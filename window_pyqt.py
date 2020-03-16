27570c7984827d1f8fafab34f65cb36287d5921a
roberta <roberta.mangiapane@gmail.com>
2020-03-16 15:11:02 +0000

    WIP UI

--- window_pyqt.py
+++ window_pyqt.py
@@ -8,12 +8,15 @@ app.setStyle('Macintosh')
 
 class GameWindow(QWidget):
 
-    def __init__(self, window_title, height, width, player_name):
+    def __init__(self, window_title, height, width, game=Game()):
         super().__init__()
         self.window_title = window_title
         self.height = height
         self.width = width
-        self.player_name = player_name
+        # self.player_name = game.player_name
+        self.player_name = "Roberta"
+        self.computer_choice = game.computer_choice
+        self.game_result = game.winner
         self.layout = QVBoxLayout()
         self.initUI()
 
@@ -22,15 +25,15 @@ class GameWindow(QWidget):
         self.setWindowTitle(self.window_title)
         self.resize(self.height, self.width)
 
-        label_player = QLabel("<font color=white size=10>{}</font>".format(self.player_name))
+        label_player = QLabel("<font color=white size=10>{}'s choice:</font>".format(self.player_name))
         label_player.setAlignment(Qt.AlignTop)
 
         rock_button = QPushButton('Rock')
         paper_button = QPushButton('Paper')
         scissors_button = QPushButton('Scissors')
 
-        label_computer = QLabel("<font color=white size=10>Computer choice</font>")
-        label_game_result = QLabel("<font color=red size=10>Game Result</font>")
+        label_computer = QLabel("<font color=white size=10>Computer's choice: {}</font>".format(self.computer_choice))
+        label_game_result = QLabel("<font color=red size=10>Game Result:</font>")
 
         self.layout.addWidget(label_player)
         self.layout.addWidget(rock_button)
@@ -41,7 +44,7 @@ class GameWindow(QWidget):
 
 
 if __name__ == '__main__':
-    window = GameWindow("RSP Game", 1000, 700, "Roberta")
+    window = GameWindow("RSP Game", 300, 400)
     window.setLayout(window.layout)
     window.show()
     app.exec_()
