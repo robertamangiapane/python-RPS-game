837bc6e33ce920b01fd783b04d7f08bff6075b19
roberta <roberta.mangiapane@gmail.com>
2020-03-18 15:03:56 +0000

    WIP setting up layout

--- window_pyqt.py
+++ window_pyqt.py
@@ -8,43 +8,85 @@ app.setStyle('Macintosh')
 
 class GameWindow(QWidget):
 
-    def __init__(self, window_title, height, width, game=Game()):
+    def __init__(self, game=Game()):
         super().__init__()
-        self.window_title = window_title
-        self.height = height
-        self.width = width
-        # self.player_name = game.player_name
-        self.player_name = "Roberta"
-        self.computer_choice = game.computer_choice
-        self.game_result = game.winner
+        self.setWindowTitle("RSP Game")
+        self.game = game
         self.layout = QVBoxLayout()
-        self.initUI()
+        self.player_label = QLabel()
+        self.player_name_textbox = QLineEdit(self)
+        self.computer_choice = QLabel()
+        self.game_result = QLabel()
+        self.start_game_button = QPushButton()
+        self.paper_button = QPushButton()
+        self.rock_button = QPushButton()
+        self.scissors_button = QPushButton()
 
-    def initUI(self):
+        self.setup_main_window()
 
-        self.setWindowTitle(self.window_title)
-        self.resize(self.height, self.width)
+    def setup_main_window(self):
+        self.resize(200, 170)
 
-        label_player = QLabel("<font color=white size=10>{}'s choice:</font>".format(self.player_name))
-        label_player.setAlignment(Qt.AlignTop)
+        self.player_label.setText("<font color=white size=5>Insert your name and play VS the Computer</font>")
+        self.player_label.setAlignment(Qt.AlignTop)
+        self.layout.addWidget(self.player_label)
 
-        rock_button = QPushButton('Rock')
-        paper_button = QPushButton('Paper')
-        scissors_button = QPushButton('Scissors')
+        self.player_name_textbox.move(30, 70)
+        self.player_name_textbox.resize(350, 35)
+        self.player_name_textbox.setAlignment(Qt.AlignCenter)
+        font = self.player_name_textbox.font()
+        font.setPointSize(23)
+        self.player_name_textbox.setFont(font)
 
-        label_computer = QLabel("<font color=white size=10>Computer's choice: {}</font>".format(self.computer_choice))
-        label_game_result = QLabel("<font color=red size=10>Game Result:</font>")
+        self.start_game_button.setText('Start New Game')
+        self.start_game_button.clicked.connect(self.setup_game_window)
+        self.layout.addWidget(self.start_game_button)
 
-        self.layout.addWidget(label_player)
-        self.layout.addWidget(rock_button)
-        self.layout.addWidget(paper_button)
-        self.layout.addWidget(scissors_button)
-        self.layout.addWidget(label_computer)
-        self.layout.addWidget(label_game_result)
+    def setup_game_window(self):
+        self.resize(400, 200)
+        self.game.player_name = self.player_name_textbox.text()
+        self.player_label.setText("<font color=white size=10>Hello {}, "
+                                  "choose one option:</font>".format(self.player_name_textbox.text()))
+
+        self.player_name_textbox.hide()
+        self.start_game_button.hide()
+        self.rock_button.setText('Rock')
+        self.paper_button.setText('Paper')
+        self.scissors_button.setText('Scissors')
+
+        self.rock_button.setObjectName('Rock')
+        self.paper_button.setObjectName('Paper')
+        self.scissors_button.setObjectName('Scissors')
+
+        self.layout.addWidget(self.rock_button)
+        self.layout.addWidget(self.paper_button)
+        self.layout.addWidget(self.scissors_button)
+
+        self.rock_button.clicked.connect(self.on_choice_clicked)
+        self.paper_button.clicked.connect(self.on_choice_clicked)
+        self.scissors_button.clicked.connect(self.on_choice_clicked)
+
+    def on_choice_clicked(self):
+        sending_button = self.sender()
+        self.game.player_choice = self.game.player_turn_option(sending_button.objectName())
+        self.player_label.setText("<font color=white size=10>You chose: {}</font>".format(self.game.player_choice))
+        self.computer_choice.setText("<font color=white size=10>Computer chose: {}</font>".format(self.game.computer_turn_option()))
+        self.game_result.setText("<font color=red size=10>Game Result: {}</font>".format(self.game.declare_winner()))
+
+        self.layout.addWidget(self.computer_choice)
+        self.layout.addWidget(self.game_result)
+
+        self.rock_button.hide()
+        self.paper_button.hide()
+        self.scissors_button.hide()
+
+        self.layout.addWidget(self.start_game_button, alignment=Qt.AlignBottom)
+        self.start_game_button.show()
+        self.start_game_button.clicked.connect(self.setup_game_window)
 
 
 if __name__ == '__main__':
-    window = GameWindow("RSP Game", 300, 400)
+    window = GameWindow()
     window.setLayout(window.layout)
     window.show()
     app.exec_()
